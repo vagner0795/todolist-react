c904a9f81ad8585f29ff8fa76ec47eb346262f90
vagner0795 <vagner-se@hotmail.com>
2023-08-07 11:31:07 -0300

    feat: created list, add uuid to create keys

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
+import { v4 as uuidv4 } from "uuid";
+
 function App() {
+  const list = [
+    { id: uuidv4(), task: "Regar as Plantas" },
+    { id: uuidv4(), task: "Lavar a Louça" },
+    { id: uuidv4(), task: "Comprar o Pão" },
+    { id: uuidv4(), task: "Tomar Café" },
+    { id: uuidv4(), task: "Ver Aulas" },
+  ];
+
+  console.log(list);
+
   function inputMudou(event) {
     console.log(event.target.value);
+    list.push({ id: uuidv4(), task: event.target.value });
+
+    console.log(list);
   }
   function butaoClicado() {
     console.log("fui clicado");
@@ -15,9 +30,9 @@ function App() {
       />
       <button onClick={butaoClicado}>Adicionar</button>
       <ul>
-        <li>Regar as Plantas</li>
-        <li>Comprar Pão</li>
-        <li>Assistir as Aulas</li>
+        {list.map((item) => (
+          <li key={item.id}>{item.task}</li>
+        ))}
       </ul>
     </div>
   );
